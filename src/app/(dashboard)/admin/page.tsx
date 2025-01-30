b721912caece87f2894372f14825df2c55bd9d1b
Bao Vo <giabaovo123456@gmail.com>
2025-01-30 21:41:23 +0700

    Count Chart And Attendance Chart For Admin Page Design

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,4 +1,6 @@
 import UserCard from "@/components/UserCard";
+import CountChart from "@/components/CountChart";
+import AttendanceChart from "@/components/AttendanceChart";
 
 const AdminPage = () => {
   return (
@@ -11,6 +13,17 @@ const AdminPage = () => {
           <UserCard type="teacher" />
           <UserCard type="parent" />
           <UserCard type="staff" />
+        </div>
+        {/* MIDDLE CHARTS */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* COUNT CHART */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* ATTENDANCE CHART */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart/>
+          </div>
         </div>
       </div>
       {/* RIGHT */}
