e7f4eaf254816be40348a679fb22197906795e2c
Bao Vo <giabaovo123456@gmail.com>
2025-01-30 21:12:57 +0700

    User Card For Admin Page Design

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,23 @@
-const AdminPage = () => {
-    return (
-        <div className="">Admin Page</div>
-    )
-}
+import UserCard from "@/components/UserCard";
 
-export default AdminPage
+const AdminPage = () => {
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* USER CARDS */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+      </div>
+    </div>
+  );
+};
+
+export default AdminPage;
