2cd086b32ff69465e32687ea9831d6f9431d9756
Bao Vo <giabaovo123456@gmail.com>
2025-01-30 20:58:42 +0700

    Responsive Navbar Design

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 
 export default function DashboardLayout({
   children,
@@ -23,6 +24,7 @@ export default function DashboardLayout({
       </div>
       {/* RIGHT */}
       <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+          <Navbar />
       </div>
     </div>
   );
