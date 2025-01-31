cd66320c1c103bab5991cb5fd8990f46c61c091e
Bao Vo <giabaovo123456@gmail.com>
2025-01-31 14:30:13 +0700

    Student Page Design

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import {role} from "@/lib/data";
 
 const menuItems = [
   {
@@ -124,7 +125,9 @@ const Menu = () => {
           <span className="hidden lg:block text-gray-400 font-light my-4">
             {i.title}
           </span>
-          {i.items.map((item) => (
+          {i.items.map((item) => {
+            if (item.visible.includes(role)) {
+              return (
                 <Link
                   href={item.href}
                   key={item.label}
@@ -133,7 +136,9 @@ const Menu = () => {
                   <Image src={item.icon} alt="" width={20} height={20} />
                   <span className="hidden lg:block">{item.label}</span>
                 </Link>
-          ))}
+              );
+            }
+          })}
         </div>
       ))}
     </div>
